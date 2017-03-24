959f08075d22222440b1e7b820c2ceecde83764f
Alixia <lilou@DESKTOP-RUAU0LT.mshome.net>
2017-03-24 11:18:52 +0100

    ajout moteur

---
+++ lejos/src/Noname/Moteur.java
@@ -0,0 +1,47 @@
+package Noname;
+
+public class Moteur implements APIMoteur {
+
+	@Override
+	public void setSpeed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reculer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void avancer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tournerDroite() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tournerGauche() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fermer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ouvrir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
