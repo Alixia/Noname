b0372a8003c0578d1e6b3467617b8ce1d0b9aa8e
NeoKa4ra <navarro.romain.j@gmail.com>
2017-04-19 16:51:33 +0200

    merging branches

---
+++ lejos/src/Noname/Camera.java
@@ -0,0 +1,70 @@
+package Noname;
+ 
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import lejos.hardware.Button;
+
+public class Camera {
+	public static void main(String args[]) {
+
+		try {
+			int port = 8888;
+
+			// Create a socket to listen on the port.
+			DatagramSocket dsocket = new DatagramSocket(port);
+
+			// Create a buffer to read datagrams into. If a
+			// packet is larger than this buffer, the
+			// excess will simply be discarded!
+			byte[] buffer = new byte[2048];
+
+			// Create a packet to receive data into the buffer
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			boolean rester = true;
+			int nbIteration = 100;
+			// Now loop forever, waiting to receive packets and printing them.
+			while (rester) {
+				// Wait to receive a datagram
+				dsocket.receive(packet);
+
+				// Convert the contents to a string, and display them
+				//System.out.println("avant");
+				String msg = new String(buffer, 0, packet.getLength());
+				//System.out.println("apres");
+				// System.out.println(packet.getAddress().getHostName() + ": "
+				// + msg);
+
+				String[] palets = msg.split("\n");
+
+				for (int i = 0; i < palets.length; i++) {
+					String[] coord = palets[i].split(";");
+					int x = Integer.parseInt(coord[1]);
+					int y = Integer.parseInt(coord[2]);
+					int index = Integer.parseInt(coord[0]);
+
+					System.out.println(Integer.toString(index) + ":" + Integer.toString(x) + " / " + Integer.toString(y));
+				Thread.sleep(1000);
+				}
+
+				// Reset the length of the packet before reusing it.
+				packet.setLength(buffer.length);
+				
+				nbIteration--;
+				if(nbIteration <= 0 ){
+					rester = false;
+				}
+			}
+			
+			while(true){
+				
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+			while(true){
+				
+			}
+		}
+	}
+}
--- lejos/src/Noname/Test/TestCouleurs.java
+++ lejos/src/Noname/Test/TestCouleurs.java
@@ -1,4 +1,5 @@
 package Noname.Test;
+ 
 
 import Noname.Capteurs;
 import Noname.Outils.Couleur;
@@ -10,26 +11,26 @@ public class TestCouleurs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Capteurs capteurs = new Capteurs();
-		Screen screen = new Screen();
-		InputHandler input = new InputHandler(screen);
+		//Screen screen = new Screen();
+		//InputHandler input = new InputHandler(screen);
 
-		for (Couleur c : Couleur.values()) {
+		capteurs.calibration();
+		/*for (Couleur c : Couleur.values()) {
 			System.out.println("couleur = " + c.name());
 			input.waitAny();
 			capteurs.calibrerCouleur(c);
 			System.out.println("tableur calibrationnage" + tableauString(capteurs.getCalibration()));
-
-		}
+		}*/
 		// the end
 
 	}
 
-	private static String tableauString(float[][] tab) {
+/*	private static String tableauString(float[][] tab) {
 		String buffer = "";
 		for (int i = 0; i < Couleur.values().length; i++) {
 			buffer += " " + tab[i][0];
 		}
 		return buffer;
 	}
-
+*/
 }
