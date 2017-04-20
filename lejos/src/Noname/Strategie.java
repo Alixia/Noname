caedd942330ad949277ecc57dd9d58f5f8ac8131
Alixia <lilou@192.168.1.11>
2017-04-20 13:56:40 +0200

    modif strat

--- lejos/src/Noname/Strategie.java
+++ lejos/src/Noname/Strategie.java
@@ -22,66 +22,6 @@ public class Strategie {
 	
 	public void dirigerVersPalet(Point positionRobot, Point positionPalet){
 		moteurs.arreter();
-//		if(positionPalet.y == positionRobot.y){
-//			if(positionPalet.x > positionRobot.x){
-//				if(moteurs.angle() >= 85 && moteurs.angle() <= 95){
-//					moteurs.demiTour();
-//				}
-//			}
-//		}else{
-			boolean face;
-			if(positionRobot.y < positionPalet.y){
-				moteurs.revenirAngleInitial(true, 200);
-				face = true;
-			}else{
-				moteurs.revenirAngleInitial(false, 200);
-				face = false;
-			}
-			double tangenteTeta = Math.abs(positionPalet.x - positionRobot.x)/Math.abs(positionPalet.y-positionRobot.y);
-			double teta = Math.atan(tangenteTeta);
-			
-			if(positionRobot.x < positionPalet.x){
-				if(face){
-					moteurs.tourner(1*Math.toDegrees(teta), false, 120);
-				}else{
-					moteurs.tourner(-1*Math.toDegrees(teta), false, 120);
-				}
-				
-			}else{
-				if(face){
-					moteurs.tourner(-1*Math.toDegrees(teta), false, 120);
-				}else{
-					moteurs.tourner(1*Math.toDegrees(teta), false, 120);
-				}
-			}
-		//}
-	}
-	
-}
-package Noname;
-
-import java.awt.Point;
-
-public class Strategie {
-
-	private Capteurs capteur;
-	private Moteurs moteurs;
-	private Pince pince;
-	
-	public Strategie (Capteurs ca, Moteurs m, Pince p){
-		this.capteur = ca;
-		this.moteurs = m;
-		this.pince = p;
-	}
-	
-	public void intialisation(){
-		pince.calibration();
-		//capteur.calibration();
-		moteurs.calibration();
-	}
-	
-	public void dirigerVersPalet(Point positionRobot, Point positionPalet){
-		moteurs.arreter();
 		if(positionPalet.y == positionRobot.y){
 			moteurs.revenirAngleInitial(true, 120);
 			if(positionRobot.x < positionPalet.x){
--- lejos/src/Noname/Test/TestPince.java
+++ lejos/src/Noname/Test/TestPince.java
@@ -3,7 +3,7 @@ package Noname.Test;
 import Noname.Pince;
 import lejos.utility.Delay;
 
-public class PinceTest {
+public class TestPince {
 
 	public static void main(String[] args) {
 		Pince P = new Pince(500);
