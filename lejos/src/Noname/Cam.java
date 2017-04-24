d8ecc42631987fc5fc1be28bb0d9a005c23b4293
Anthony Papasergio <papasergio.anthony@gmail.com>
2017-04-24 23:15:39 +0200

    Ajout de la detection de collicsion

--- lejos/src/Noname/Cam.java
+++ lejos/src/Noname/Cam.java
@@ -6,17 +6,23 @@ public class Cam {
 
 	private int[][] palets;
 	private int[][] robots;
+	private boolean[][] tabColisions;
+
+	private int colision;
+	private int distColision;
 
 	public Cam() {
 		palets = new int[9][3];
 		robots = new int[2][3];
+		colision = 0;
+		distColision = 15;
 
 		init();
 	}
 
 	private String changeMsg(String msg) {
 		// String msg =
-		// "0;100;100\n1;200;100\n2;300;100\n3;0;200\n4;200;100\n5;200;200\n6;200;300\n7;400;200\n8;300;100\n9;300;200\n10;300;300";
+		//String msg = "0;50;100\n1;50;150\n2;50;200\n3;100;50\n4;100;100\n5;100;150\n6;100;200\n7;100;250\n8;150;100\n9;150;150\n10;150;200";
 		String[] buff = msg.split("\n");
 		String Nextmsg = "";
 
@@ -28,9 +34,9 @@ public class Cam {
 			int x = Integer.parseInt(coord[1]);
 			int y = Integer.parseInt(coord[2]);
 			int index = Integer.parseInt(coord[0]);
-			if (index == 3 || index == 7) {
+			if (index == 3) {
 				x += r.nextInt(2 * amplitude) - amplitude;
-				y += r.nextInt(2 * amplitude) - amplitude;
+				y += 10;
 			}
 
 			Nextmsg += index + ";" + x + ";" + y + "\n";
@@ -41,7 +47,7 @@ public class Cam {
 
 	private void init() {
 		// index;x;y robot1
-		String msg = "0;100;100\n1;200;100\n2;300;100\n3;0;200\n4;200;100\n5;200;200\n6;200;300\n7;400;200\n8;300;100\n9;300;200\n10;300;300";
+		String msg = "0;50;100\n1;50;150\n2;50;200\n3;100;50\n4;100;100\n5;100;150\n6;100;200\n7;100;250\n8;150;100\n9;150;150\n10;150;200";
 		String[] buff = msg.split("\n");
 
 		int irobot = 0;
@@ -53,7 +59,7 @@ public class Cam {
 			int y = Integer.parseInt(coord[2]);
 			int index = Integer.parseInt(coord[0]);
 
-			if (x == 0 || x == 400) {
+			if (y == 50 || y == 250) {
 				robots[irobot][0] = index;
 				robots[irobot][1] = x;
 				robots[irobot][2] = y;
@@ -74,6 +80,15 @@ public class Cam {
 		boolean[] bpalets = new boolean[9];
 		boolean[] brobots = new boolean[2];
 
+		tabColisions = new boolean[11][11];
+
+		colision = 0;
+		for (int i = 0; i < 11; i++) {
+			for (int j = 0; j < 11; j++) {
+				tabColisions[i][j] = false;
+			}
+		}
+
 		for (int i = 0; i < bpalets.length; i++) {
 			bpalets[i] = false;
 		}
@@ -85,7 +100,7 @@ public class Cam {
 
 		int irobot = 0;
 		int ipalet = 0;
-
+		System.out.println("nbre donnees reçues: " + buff.length);
 		for (int i = 0; i < buff.length; i++) {
 			String[] coord = buff[i].split(";");
 			int x = Integer.parseInt(coord[1]);
@@ -102,6 +117,19 @@ public class Cam {
 
 				current = Math.sqrt(newx * newx + newy * newy);
 				// System.out.println("currentP = " + current );
+
+				if (current <= distColision) {
+					System.out.println("palet colision["+j+"]["+i+"] : current = "+current);
+					System.out.println("newx="+newx + " newy=" + newy);
+					System.out.println("i: "+ index + " recu: " + x + "/" + y +" - palets: i:" +palets[j][0]+ " " + palets[j][1] + "/" + palets[j][2]);
+					System.out.println(affichePalets());
+
+					colision++;
+					tabColisions[j][i] = true;
+				}else{
+					//System.out.println("palets pas colision: "+current);
+				}
+
 				if (max > current && bpalets[j] == false) {
 					max = current;
 					indexMax = j;
@@ -112,8 +140,23 @@ public class Cam {
 				int newx = x - robots[j][1];
 				int newy = y - robots[j][2];
 
+				
 				current = Math.sqrt(newx * newx + newy * newy);
 				// System.out.println("currentR = " + current );
+				
+				if (current <= distColision) {
+					System.out.println("robot colision["+j+"]["+i+"] : current = "+current);
+					System.out.println(x + " - " + robots[j][1] + " - " + y +" - " + robots[j][2]);
+					System.out.println("newx="+newx + " newy=" + newy);
+					System.out.println("i: "+ index + " recu: " + x + "/" + y +" - robots: i:" +robots[j][0]+ " " + robots[j][1] + "/" + robots[j][2]);
+					System.out.println(afficheRobots());
+					colision++;
+					tabColisions[palets.length + j][i] = true;
+				}else{
+					//System.out.println("robots pas colision: "+current);
+				}
+				
+				
 				if (max >= current && brobots[j] == false) {
 					max = current;
 					indexMax = j;
@@ -139,6 +182,23 @@ public class Cam {
 
 		}
 
+		if (colision > 11) {
+			System.out.println("COLISION! " + colision);
+			System.out.println("palets:\n" + affichePalets());
+			System.out.println("robots:\n" + afficheRobots());
+			System.out.println("robots:\n" + afficheColisions());
+			
+			pressAnyKeyToContinue();
+		}
+
+	}
+
+	private void pressAnyKeyToContinue() {
+		// System.out.println("Press any key to continue...");
+		try {
+			System.in.read();
+		} catch (Exception e) {
+		}
 	}
 
 	public int[][] getPalets() {
@@ -147,6 +207,24 @@ public class Cam {
 
 	public int[][] getRobots() {
 		return robots;
+	}
+
+	public String afficheColisions() {
+		String buffer = "";
+		int lignes = tabColisions.length;
+		int colonnes = tabColisions[0].length;
+		for (int i = 0; i < lignes; i++) {
+			for (int j = 0; j < colonnes; j++) {
+				if(tabColisions[i][j]){
+					buffer += "1 ";
+				}
+				else{
+					buffer += "0 ";
+				}
+			}
+			buffer += "\n";
+		}
+		return buffer;
 	}
 
 	public String affichePalets() {
@@ -173,11 +251,11 @@ public class Cam {
 		for (int j = 0; j < 1000; j++) {
 			Cam c = new Cam();
 
-			String msg = "0;100;100\n1;200;100\n2;300;100\n3;0;200\n4;200;100\n5;200;200\n6;200;300\n7;400;200\n8;300;100\n9;300;200\n10;300;300";
+			String msg = "0;50;100\n1;50;150\n2;50;200\n3;100;50\n4;100;100\n5;100;150\n6;100;200\n7;100;250\n8;150;100\n9;150;150\n10;150;200";
 			// System.out.println(c.affichePalets());
 			// System.out.println(c.afficheRobots());
 
-			for (int i = 0; i < 1000; i++) {
+			for (int i = 0; i < 10; i++) {
 				msg = c.changeMsg(msg);
 
 				c.MajCoords(msg);
