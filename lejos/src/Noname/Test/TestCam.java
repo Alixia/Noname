94bbfb06200716a86a07aef1377cfb6150efa597
Alixia <lilou@DESKTOP-RUAU0LT>
2017-04-27 14:41:30 +0200

    merge

--- lejos/src/Noname/Test/TestCam.java
+++ lejos/src/Noname/Test/TestCam.java
@@ -12,7 +12,7 @@ public class TestCam {
 
 	public static void main(String[] args) {
 		Moteurs m = new Moteurs();
-		Pince p = new Pince();
+		Pince p = new Pince(true);
 		Capteurs c = new Capteurs();
 		Strategie s = new Strategie(c, m, p);
 		InputHandler ih = new InputHandler(new Screen());
