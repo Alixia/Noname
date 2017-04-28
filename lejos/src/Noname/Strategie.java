7ec30701ff0270c6138cc4225ab3cb3f62798b61
Alixia <lilou@DESKTOP-RUAU0LT>
2017-04-28 12:08:20 +0200

    modif strat

--- lejos/src/Noname/Strategie.java
+++ lejos/src/Noname/Strategie.java
@@ -35,9 +35,13 @@ public class Strategie {
 		this.pince = p;
 		etat = MachineEtat.NOPALLET;
 
+		System.out.println("debut thread");
 		cam = new Cam();
+		
+		System.out.println("debut cam");
 		ThreadCam = new Thread(cam);
 	
+		System.out.println("debut init");
 		intialisation();
 	}
 	
@@ -75,11 +79,12 @@ public class Strategie {
 		} catch (ClassNotFoundException | IOException e) {
 			e.printStackTrace();
 		}
-		
+		System.out.println("lancer cam");
 		lancerCam();
 		
 		tabPallet = cam.getPalets();
 		tabRobot = cam.getRobots();
+		System.out.println("calibration");
 		calibration();
 	}
 	
@@ -89,24 +94,28 @@ public class Strategie {
 		// Position du robot
 		Point pos = new Point(tabRobot[indiceRobot][x], tabRobot[indiceRobot][y]);
 		// Decalage du robot pour esquiver les palets
-		int delta = (tabRobot[indiceAdverse][x] > tabRobot[indiceRobot][x])? -15 : 15;
+		int delta = (tabRobot[indiceAdverse][x] > tabRobot[indiceRobot][x])? -25 : 25;
 		// Destination vers les cages
-		Point dest = new Point(tabRobot[indiceRobot][x]+delta, 150);
-		System.out.println(dest + "   " + pos);
+		Point dest = new Point(tabRobot[indiceRobot][x]+delta, tabRobot[indiceRobot][y]);
+		System.out.println("dest : " + dest + "     pos : " + pos+  "     angle : " + moteurs.getAngle());
 		seDirigerVers(pos,dest);
 		moteurs.avancer();
-		Delay.msDelay(1000);
+		Delay.msDelay(1200);
 		moteurs.arreter();
 		miseAJour();
 		pos.move(tabRobot[indiceRobot][x], tabRobot[indiceRobot][y]);
 		dest.move(tabRobot[indiceRobot][x], yCage);
-		System.out.println(dest + "   " + pos);
+		System.out.println(dest + "   " + pos + "     angle : " + moteurs.getAngle());
 		seDirigerVers(pos, dest);
 		// Tant qu'on a pas traverse la ligne blanche
-		while (!capteur.getCurrentColor().equals(Couleur.blanc)) {
+		boolean enCours = true;
+		while (enCours) {
 			moteurs.avancer();
-			while(!capteur.getCurrentColor().equals(Couleur.blanc) && ((Math.abs(tabRobot[indiceAdverse][x] - tabRobot[indiceRobot][x]) > 20)  || (Math.abs(tabRobot[indiceAdverse][y] - tabRobot[indiceRobot][y]) > 40))){
-				Delay.msDelay(200);
+			while(enCours && ((Math.abs(tabRobot[indiceAdverse][x] - tabRobot[indiceRobot][x]) > 20)  || (Math.abs(tabRobot[indiceAdverse][y] - tabRobot[indiceRobot][y]) > 40))){
+				Delay.msDelay(10);
+				if(!capteur.getCurrentColor().equals(Couleur.blanc)){
+					enCours = false;
+				}
 				miseAJour();
 			}
 			moteurs.arreter();
@@ -165,9 +174,7 @@ public class Strategie {
 		int indicePalletPlusProche = 0;
 		double distancePrec = 5000;
 		for (int i = 0; i < tabPallet.length; i++) {
-			if ((tabPallet[i][y] > 50) && (tabPallet[i][y] < 250)
-					&& !pointsEgaux(new Point(tabPallet[i][x], tabPallet[i][y]),
-							new Point(tabRobot[indiceAdverse][x], tabRobot[indiceAdverse][y]))) {
+			if ((tabPallet[i][y] > yMin + 15) && (tabPallet[i][y] < yMax - 15)) {
 				double distanceEnCours = Math.sqrt(Math.pow(tabPallet[i][x] - tabRobot[indiceRobot][x], 2)
 						+ Math.pow(tabPallet[i][y] - tabRobot[indiceRobot][y], 2));
 				if (distancePrec > distanceEnCours) {
@@ -212,7 +219,7 @@ public class Strategie {
 	public void miseAJour() {
 		int[][] newTabPallet = cam.getPalets();
 		int[][] newTabRobot = cam.getRobots();
-		afficherTableaux();
+		//afficherTableaux();
 		
 		if((Math.abs(newTabPallet[indiceRobot][x] - tabPallet[indiceRobot][x]) < 10) || (Math.abs(newTabPallet[indiceRobot][y] - tabPallet[indiceRobot][y]) < 10)){
 			double teta = 0;
@@ -225,27 +232,27 @@ public class Strategie {
 			}
 			else{
 				double tangenteTeta = Math.abs((newTabRobot[indiceRobot][x] - tabRobot[indiceRobot][x]) / (newTabRobot[indiceRobot][y] - tabRobot[indiceRobot][y]));
-				teta = Math.atan(tangenteTeta);
+				teta = Math.atan(tangenteTeta);				
 			}
-			moteurs.setAngle(teta);
+			//moteurs.setAngle(teta);
 			System.arraycopy(newTabPallet, 0, tabPallet, 0, tabPallet.length);
 			System.arraycopy(newTabRobot, 0, tabRobot, 0, tabRobot.length);
 		}
 	}
 
 	public void rentrerALaMaison() {
-		seDirigerVers(new Point(tabRobot[indiceRobot][x],  tabRobot[indiceRobot][y]), new Point(100,yCageAdverse));
+		seDirigerVers(new Point(tabRobot[indiceRobot][x],  tabRobot[indiceRobot][y]), new Point(100,yCage));
 		moteurs.avancer();
 
 		// prendre en compte les erreurs potentielles du aux angles
 		// prendre en compte les obstacle
 		while (!capteur.getCurrentColor().equals(Couleur.blanc)) {
-			Delay.msDelay(200);
+			Delay.msDelay(100);
 		}
 		moteurs.arreter();
 		pince.ouvrirPince();
 		moteurs.reculer();
-		Delay.msDelay(200);
+		Delay.msDelay(500);
 		moteurs.arreter();
 
 		miseAJour();
@@ -269,9 +276,14 @@ public class Strategie {
 		}
 		
 	}
+	public void premiereEtape(){
+		allerChercherPallet(detecterPlusProchePallet());
+		ramenerPremierPalet();
+	}
 
 	public void run() {
 		Point pallet;
+		premiereEtape();
 		while (true) {
 			switch (etat) {
 			case NOPALLET:
@@ -285,5 +297,8 @@ public class Strategie {
 				etat = MachineEtat.NOPALLET;
 			}
 		}
+	}
+	public void stop(){
+		ThreadCam.stop();
 	}
 }
