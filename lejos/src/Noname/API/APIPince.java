4f088dfe67bbca5b44890cfbfcd38fcbdd26d947
Romain <Romain@130.190.28.1>
2017-04-28 10:09:39 +0200

    Ajout de précisions dans APIpince

--- lejos/src/Noname/API/APIPince.java
+++ lejos/src/Noname/API/APIPince.java
@@ -1,6 +1,11 @@
 package Noname.API;
 
 public interface APIPince {
+	/* Il faut savoir que le constructeur prend un booleen
+	 * qui represente si la pince est ouverte ou non
+	 * (envoyer true si elle est ouverte, false sinon).
+	 * Si la pince est fermee, il va tenter de l'ouvrir.
+	 */
 	
 	/**
 	 * effectue la calibration pour l'ouverture et la fermeture des pinces
