4088f2998d0bcb3fde1535ea2bf72cee1a2ce88c
Romain <Romain@130.190.28.1>
2017-04-28 10:15:53 +0200

    Ajout des commentaires des tests faits dans TestPince

--- lejos/src/Noname/Test/TestPince.java
+++ lejos/src/Noname/Test/TestPince.java
@@ -5,16 +5,21 @@ import lejos.utility.Delay;
 
 public class TestPince {
 
+	// Test de la pince basique
 	public static void main(String[] args) {
 		Pince P = new Pince(true);
+		// Test de la calibration des pinces sur le palet
         P.calibration();
-//		Delay.msDelay(1000);
-//		for (int i = 0; i < 10; i++) {
-//			P.fermerPince();
-//			P.fermerPince();
-//			P.ouvrirPince();
-//			P.ouvrirPince();
-//		}
+		Delay.msDelay(1000);
+		// Dans ce test, nous essayons d'ouvrir et de fermer les pinces plusieurs fois
+		// d'affilee, puisque le programme est cense gerer ce genre de situations
+		// afin de ne pas se retrouver dans une situation non voulue
+		for (int i = 0; i < 10; i++) {
+			P.fermerPince();
+			P.fermerPince();
+			P.ouvrirPince();
+			P.ouvrirPince();
+		}
        
 	}
 
